de23f8970526519d1dfaf0a4f1db5e356b1d2b41
habiba-darwish <habibadarwish2004@gmail.com>
2024-05-02 18:50:33 +0300

    Sticky Filter Bar / Side Bar Cart

--- Javascript/ShoppAll.js
+++ Javascript/ShoppAll.js
@@ -1,40 +1,50 @@
 
-let cartCount = 0;
-function updateCart() {
-    const cartCountElement = document.querySelector('.cart-count');
-    const checkoutTotalElement = document.querySelector('.checkout-total');
+// let cartCount = 0;
+// function updateCart() {
+//     const cartCountElement = document.querySelector('.cart-count');
+//     const checkoutTotalElement = document.querySelector('.checkout-total');
 
  
-    cartCountElement.textContent = cartCount;
-    let totalPrice = 0; 
+//     cartCountElement.textContent = cartCount;
+//     let totalPrice = 0; 
 
-    checkoutTotalElement.textContent = `$${totalPrice.toFixed(2)}`;
-}
+//     checkoutTotalElement.textContent = `$${totalPrice.toFixed(2)}`;
+// }
 
 
-const checkoutBtn = document.querySelector('.checkout-btn');
-checkoutBtn.addEventListener('click', () => {
+// const checkoutBtn = document.querySelector('.checkout-btn');
+// checkoutBtn.addEventListener('click', () => {
   
-    window.location.href = 'checkout.html';
-});
+//     window.location.href = 'checkout.html';
+// });
 
 
-const addToCartButtons = document.querySelectorAll('.add-to-cart-btn');
+// const addToCartButtons = document.querySelectorAll('.add-to-cart-btn');
 
 
-addToCartButtons.forEach(addToCartButton => {
-    addToCartButton.addEventListener('click', () => {
+// addToCartButtons.forEach(addToCartButton => {
+//     addToCartButton.addEventListener('click', () => {
       
-        cartCount++;
+//         cartCount++;
 
   
-        updateCart();
-    });
-});
+//         updateCart();
+//     });
+// });
 
 
-const cartIcon = document.getElementById('cart-icon');
-cartIcon.addEventListener('click', () => {
+// const cartIcon = document.getElementById('cart-icon');
+// cartIcon.addEventListener('click', () => {
     
-    console.log('Cart icon clicked!');
+//     console.log('Cart icon clicked!');
+// });
+// ---------------------------------------------------------------------------------------------------------
+const addToCartButton = document.querySelector('.add-to-cart-btn');
+const sidebar = document.querySelector('.sidebar');
+
+// Add click event listener to "Add to Cart" button
+addToCartButton.addEventListener('click', () => {
+    // Toggle the 'hidden' class on the sidebar to show/hide it
+    // sidebar.classList.toggle('hidden');
+    sidebar.classList.remove('hidden');
 });
