d41a71ba98a2dc20e45144775b3bec9161381563
hala-155 <140650349+hala-155@users.noreply.github.com>
2024-05-04 04:25:13 +0300

    css + js
    added css, js file w image changes

---
+++ Javascript/middleEast.js
@@ -0,0 +1,47 @@
+//scroll effect
+
+function isInViewport(element){
+    const rect= element.getBoundingClientRect();
+    console.log(rect);
+    return(
+        rect.top>=0 &&
+        rect.left >=0 &&
+        rect.bottom<= (window.innerHeight||document.documentElement.clientHeight)&&
+        rect.right<= (window.innerWidth||document.documentElement.clientWidth)
+
+
+    );
+}
+function handleScroll(){
+
+const images= document.querySelectorAll('.images-trans');
+// let delay=0;
+console.log(images);
+images.forEach(image=>{
+    if(isInViewport(image)){
+        // setTimeout(()=>{
+        //     image.classList.add('show');
+        // },index*200);
+    //    delay+=200;
+    image.classList.add('show');
+    // image.style.opacity='1';
+    // image.style.transition='opacity 0.5s ease-in-out';
+    // image.style.transform='translateY(0)';
+    }
+});
+
+}
+document.addEventListener('DOMContentLoaded',handleScroll);
+window.addEventListener('scroll',handleScroll);
+
+//heart icon "wishlist"
+
+document.addEventListener("DOMContentLoaded",function(){
+
+    const heartIcon=document.getElementById('heartIcon');
+     heartIcon.addEventListener("click",function(){
+        this.classList.toggle("fa-regular fa-heart");
+        thhis.classList.toggle("fa-solid fa-heart");
+     });
+
+});
