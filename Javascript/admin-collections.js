153dd52de948e9c68de92242361117ab0a2b9e05
shahdcode <134103174+shahdcode@users.noreply.github.com>
2024-05-08 00:58:10 +0300

    trying to fix aligments

---
+++ Javascript/admin-collections.js
@@ -0,0 +1,12 @@
+function toggleMenu(){
+    var nav= document.querySelector('.navigation');
+    nav.style.display=nav.style.display==='block'? 'none' :'block';
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+    var indianButton = document.querySelector(".collection-button");
+
+    indianButton.addEventListener("click", function () {
+        window.location.href = "EditCollection.html";
+    });
+});
