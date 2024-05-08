eca9c0a9b1ecef1ed7b71174bfb45f6b36a77bca
hayawalid <hayawalid640@gmail.com>
2024-05-08 13:03:10 +0200

    Update Quiz.js

--- Javascript/Quiz.js
+++ Javascript/Quiz.js
@@ -1,31 +1,25 @@
 function selectOption(button) {
-    // Remove 'active' class from all buttons
     var buttons = document.querySelectorAll('.response');
     buttons.forEach(function(btn) {
         if (btn !== button) {
             btn.classList.remove('active');
         }
     });
-    // Toggle 'active' class for the clicked button
     button.classList.toggle('active');
 }
 
 function showNextQuestion(nextQuestionId) {
-    // Check if an option is selected
     var selectedOption = document.querySelector('.response.active');
     if (!selectedOption) {
         alert('Please select an option.');
         return;
     }
 
-    // Hide current question and show next question
     document.getElementById("question1").style.display = "none";
     document.getElementById(nextQuestionId).style.display = "block";
 }
 function showNextQuestion(nextQuestionId) {
     document.getElementById("question1").style.display = "none";
     document.getElementById("question2").style.display = "none";
-    // Hide previous question
     document.getElementById(nextQuestionId).style.display = "block";
-    // Show next question
 }
